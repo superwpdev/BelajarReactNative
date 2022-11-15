6c99e0067326cf4911acc5c3389073e7e9c8cff7
FaisalRhdn <100942710+FaisalRhdn@users.noreply.github.com>
2022-11-15 17:36:04 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * Pembalik kata simple Penting Mari
- * UTS Mobile multimedia - Faisal
  * @format
  * @flow strict-local
  */
