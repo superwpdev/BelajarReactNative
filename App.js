c0c6cdce231bff035dcc8672401fd1826b48687c
dimaswahyu456 <dimaswahyu456@gmail.com>
2022-11-16 18:46:53 +0700

    Update APP Pembalik Kata

--- App.js
+++ App.js
@@ -1,112 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { View, Button, Text, TextInput} from 'react-native';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default class App extends Component {
+  state = {
+    teks : '',
+    submitteks : '',
+  }
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  formsChange = (event) => {
+    const { text } = event;
+    let processedData = text;
+    console.log(processedData);
+    this.setState({
+      teks : processedData
+    })
+    console.log("karakter = " + this.state.teks);
+  }
+                
+  submitForms = () => {
+    let data = this.state.teks;
+    this.setState({
+      submitteks : data.split('').reverse('').join('')
+    })
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  render() {
+    return(
+      <View>
+      <Text style={{fontSize:40}}>Program Membalikkan Kata</Text>
+        <View style={{padding :  10}}>          
+          <Text>Input :</Text>
+          <View style={{marginBottom : 10}}>
+            <TextInput
+              style={{ borderColor: 'gray', borderWidth: 1, padding : 5 }}
+              onChangeText={text => this.formsChange({ text })}
+              value={this.state.teks}
+            />
+          </View>
+          <View>
+            <Button title='submit' onPress={()=> this.submitForms()}></Button>
+          </View>
+          
+       </View>
+        <View style={{ padding: 10, marginTop : 20 }}>
+         <Text>Result :</Text>
+          <View style={{ borderColor: 'gray', borderWidth: 1, height : 200, padding : 5 }}>
+           <Text>{this.state.submitteks}</Text>
+         </View>
+       </View>
+      </View>
+        )
+        }
+}
