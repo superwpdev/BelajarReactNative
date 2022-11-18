c1b4eae84412ecf08ebb8d65e5ea6a6a20524153
varantino22 <varantino22@gmail.com>
2022-11-18 22:37:34 +0700

    uts

--- App.js
+++ App.js
@@ -1,46 +1,58 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
  * @format
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
-import {
-  TextInput,
-  Text,
-  View,
-  Button,
-  Alert, 
-} from 'react-native';
+ import React, { useState } from 'react';
+ import {
+   StyleSheet,
+   TextInput,
+   Text,
+   View,
+   Button,
+   Alert,
+ } from 'react-native';
+ 
+ 
+ const App = () => {
+   const [text, setText] = useState('');
+   const HeaderText = "PEMBALIK KATA";
+   const Footer = "Copyright By Kelompok 3"
 
-const App = () => {
-  const [text, setText] = useState('');
-  const onPress = () => newText => setText('');
-  return (
-    <View style={{padding: 10}}>
-    <TextInput
-      style={{height: 40, backgroundColor: '#f0f0f0'}}
-      placeholder="Type here to translate!"
-      onChangeText={newText => setText(newText)}
-      defaultValue={text}
-    />
-    <TextInput
-      style={{marginTop:10, height: 40, backgroundColor: '#f0f0f0'}}
-      placeholder="Type here to translate!"
-      onChangeText={newText => setText(newText)}
-      defaultValue={text}
-    />
-    <Text style={{padding: 10, fontSize: 42}}>
-      {text.split(' ').map((word) => word && '🍕').join(' ')}
-    </Text>
-    <Button
-        title="Press me"
-        onPress={() => setText('')}
-      />
-  </View>
-  );
-};
+   return (
+     <View style={styles.container}>
+    <Text style={{fontSize: 35, textAlign: 'center',color: '#322715',fontFamily: 'arial black', marginBottom: 10}}>
+       {HeaderText}
+     </Text>
+     <TextInput
+       style={{height: 40, backgroundColor: '#e0e1dc',marginBottom: 10}}
+       placeholder="Masukkan Kata"
+       onChangeText={newText => setText(newText)}
+       defaultValue={text}
+     />
+     <Button
+     title="Press Me"
+     color="#063970"
+     onPress={() => Alert.alert(text.split('').reverse('').join(''))} />
+     <Text style={{fontSize: 18, textAlign: 'center',color: '#322715',fontFamily: 'arial black',marginTop:360}}>
+       {Footer}
+     </Text>
+   </View>
+   
+   );
+   
+ };
+ const styles = StyleSheet.create({
+  container: {
+    paddingTop: 220,
+    padding: 20,
+    backgroundColor: '#edba91',
+    
+  },
+  
+  
+});
 
-export default App;
+ export default App;
