971a11881e8bb5641d5bf9b4a134169278088093
superwpdev <superwpdev@gmail.com>
2022-11-23 19:59:24 +0700

    header react native element ui

--- App.js
+++ App.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
 headerContainer: {
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundColor: '#397af8',
+  backgroundColor: '#ddffff',
   marginBottom: 20,
   width: '100%',
   paddingVertical: 15,
